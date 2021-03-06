2606d4213029f2b2c13ea02c70b7a158d25501ff
qianjin <qianjin@learnta.com>
2021-03-06 22:17:14 +0800

    数据库连接初始化

--- global/setting.go
+++ global/setting.go
@@ -1,9 +1,15 @@
 package global
 
-import "demo/pkg/setting"
+import (
+	"demo/pkg/setting"
+
+	"gorm.io/gorm"
+)
 
 var (
 	ServerSetting   *setting.ServerSettingS
 	AppSetting      *setting.AppSettingS
 	DatabaseSetting *setting.DatabaseSettingS
+
+	DBEngine *gorm.DB
 )
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,1 +1,47 @@
 package model
+
+import (
+	"demo/global"
+	"demo/pkg/setting"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+type Model struct {
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local",
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.Port,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime)
+
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.Logger.LogMode(logger.Info)
+	}
+
+	sqldb, err := db.DB()
+	sqldb.SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	sqldb.SetMaxOpenConns(databaseSetting.MaxOpenConns)
+
+	return db, nil
+
+}
